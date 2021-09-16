65e00ecb822663532887a7dde4628d1022a9dd2e
Vincent McKen <mcken.vince@gmail.com>
2021-09-16 23:25:45 +0000

    Implement create sale, all working

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -5,13 +5,29 @@ class Admin::SalesController < ApplicationController
   end
 
   def new
-    sale = Sale.new
+    @sale = Sale.new
+  end
+
+  def create
+    @sale = Sale.new(sale_params)
+
+    if @sale.save
+      redirect_to [:admin, :sales], notice: 'Sale created!'
+    else
+      render :new
+    end
   end
 
   def destroy
     @sale = Sale.find params[:id]
     @sale.destroy
     redirect_to [:admin, :sales], notice: 'Product deleted!'
+  end
+
+  private
+
+  def sale_params
+    params.require(:sale).permit(:name, :starts_on, :ends_on, :percent_off)
   end
 
 end
