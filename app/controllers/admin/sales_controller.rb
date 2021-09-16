35ed40d12a07628b9f9d0d527b03996023e8a435
Vincent McKen <mcken.vince@gmail.com>
2021-09-16 23:15:31 +0000

    Add destroy route to sales controller, add delete button at admin/sales to delete a sale

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -8,4 +8,10 @@ class Admin::SalesController < ApplicationController
     sale = Sale.new
   end
 
+  def destroy
+    @sale = Sale.find params[:id]
+    @sale.destroy
+    redirect_to [:admin, :sales], notice: 'Product deleted!'
+  end
+
 end
