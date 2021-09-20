cbacf3749dc3aa6076c73e8db0b8c14e66929971
Vincent McKen <mcken.vince@gmail.com>
2021-09-20 20:39:02 +0000

    Generate spec for User model

---
+++ spec/models/users_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    it 'must be created with matching "password" and "password_confirmation" fields' do
+      
+    end
+
+
+  end
+end
