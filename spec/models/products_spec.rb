b9a998e0422ee7be0bd6c3f4241f2238a5b3b629
Vincent McKen <mcken.vince@gmail.com>
2021-09-20 20:27:24 +0000

    Write tests for Product model validations

---
+++ spec/models/products_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it 'should save successfully when given all required fields' do
+      @category = Category.new(name: 'test')
+      @product = @category.products.new(name: 'product', price: 1, quantity: 10)
+      @product.save! # this will fail if product does not save successfully
+    end
+
+    it 'should return an error if name field is not given' do
+      @category = Category.new(name:'test')
+      @product = @category.products.new(price: 1, quantity: 10)
+      @product.save
+      puts @product.errors.full_messages
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'should return an error if price field is not given' do
+      @category = Category.new(name:'test')
+      @product = @category.products.new(name: 'product', quantity: 10)
+      @product.save
+      puts @product.errors.full_messages
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it 'should return an error if quantity field is not given' do
+      @category = Category.new(name:'test')
+      @product = @category.products.new(name: 'product', price: 1)
+      @product.save
+      puts @product.errors.full_messages
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it 'should return an error if category field is not given' do
+      @product = Product.new(name: 'product', price: 1, quantity: 10)
+      @product.save
+      puts @product.errors.full_messages
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end
+  end
+end
