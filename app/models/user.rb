dbb4b17570f07d77e97c73b0fe010cb6117dddfa
Vincent McKen <mcken.vince@gmail.com>
2021-09-21 01:58:35 +0000

    Write first few validation tests for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :first_name, :last_name, :email, :password, :password_confirmation, presence:true
 end
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -2,8 +2,22 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
-    it 'must be created with matching "password" and "password_confirmation" fields' do
-      
+    it 'should return an error if password and password_confirmation do not match' do
+      @user = User.new(first_name: 'john', last_name: 'doe', email: 'john@doe.com', password: 'super', password_confirmation: 'secret')
+      @user.save
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+
+    it 'should return an error if email is not given' do
+      @user = User.new(first_name: 'john', last_name: 'doe', password: 'secret', password_confirmation: 'secret')
+      @user.save
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+    it 'should return an error if first_name or last_name is not given' do
+      @user = User.new(email: 'john@doe.com', password: 'secret', password_confirmation: 'secret')
+      @user.save
+      expect(@user.errors.full_messages).to include("First name can't be blank", "Last name can't be blank")
     end
 
 
