63596ef2bb073af9ba288b7542351629ec2652b4
Vincent McKen <mcken.vince@gmail.com>
2021-09-22 19:25:39 +0000

    Update Admin controllers to all use http_basic_authentication

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -1,5 +1,6 @@
 class Admin::SalesController < ApplicationController
-
+  http_basic_authenticate_with :name => ENV['ADMIN_USER'], :password => ENV['ADMIN_PASS']
+  
   def index
     @sales = Sale.all
   end
