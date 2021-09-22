d7b8e8a23087666863bb48a65d7fa218cdc27e4d
Vincent McKen <mcken.vince@gmail.com>
2021-09-22 16:21:25 +0000

    Add user && for user validation when logging in, prevents app from crashing when nonexistent user tries to log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,12 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     # If the user exists AND the password entered is correct
-    if user == User.authenticate_with_credentials(params[:email], params[:password])
+    if user && user == User.authenticate_with_credentials(params[:email], params[:password])
       # Save the user id inside the browser cookie
       session[:user_id] = user.id
       redirect_to '/'
     else
+      puts "YOU GOT REDIRECTED!!!!!!!!!!!"
       # If user's login doesn't work, send them back to the login form
       redirect_to '/login'
     end
